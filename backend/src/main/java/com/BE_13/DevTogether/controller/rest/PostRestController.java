2a0c7f2081d77cb204e5e88ae6c4005981928b02
MiSa-JD <51509584+MiSa-JD@users.noreply.github.com>
2025-07-11 16:25:25 +0900

    Revert "벡엔드와 프론트엔드 통합 테스트 통과 PR"

--- backend/src/main/java/com/BE_13/DevTogether/controller/rest/PostRestController.java
+++ backend/src/main/java/com/BE_13/DevTogether/controller/rest/PostRestController.java
@@ -1,19 +1,12 @@
 package com.BE_13.DevTogether.controller.rest;
 
-import com.BE_13.DevTogether.dto.request.PostCreateRequest;
-import com.BE_13.DevTogether.dto.PostDetailResponse;
-import com.BE_13.DevTogether.dto.response.PageInfo;
-import com.BE_13.DevTogether.dto.response.PostCreateResponse;
-import com.BE_13.DevTogether.dto.response.PostListResponse;
-import com.BE_13.DevTogether.dto.response.PostSummaryResponse;
-import com.BE_13.DevTogether.entity.posts.Post;
+import com.BE_13.DevTogether.dto.PostCreateRequest;
+import com.BE_13.DevTogether.dto.PostResponse;
 import com.BE_13.DevTogether.service.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * 게시판(Board) API 컨트롤러 클래스이다.
@@ -37,12 +30,9 @@ public class PostRestController {
      * @return 게시글 등록
      */
     @PostMapping
-    public ResponseEntity<PostCreateResponse> createPost(@RequestBody PostCreateRequest request) {
-
-        Long userId = request.author().id();
-        Long postId = postService.createPost(request, userId);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(new PostCreateResponse(postId));
+    public ResponseEntity<PostResponse> createPost(@RequestBody PostCreateRequest request) {
+        PostResponse response = postService.createPost(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     /**
@@ -53,17 +43,11 @@ public class PostRestController {
      * @return 등록된 전체 게시글
      */
     @GetMapping
-    public ResponseEntity<PostListResponse> findAll(
+    public ResponseEntity<Page<PostResponse>> getPostList(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int limit) {
-
-        Page<PostSummaryResponse> postPage = postService.findAll(page, limit);
-
-        List<PostSummaryResponse> posts = postPage.getContent();
-        PageInfo pageInfo = new PageInfo(postPage.getNumber() + 1, postPage.getTotalPages());
-
-        PostListResponse response = new PostListResponse(posts, pageInfo);
-        return ResponseEntity.ok(response);
+        Page<PostResponse> responses = postService.findAll(page, limit);
+        return ResponseEntity.ok(responses);
     }
 
     /**
@@ -73,8 +57,8 @@ public class PostRestController {
      * @return 해당 게시글 조회
      */
     @GetMapping("/{postId}")
-    public ResponseEntity<PostDetailResponse> findById(@PathVariable Long postId) {
-        PostDetailResponse response = postService.findById(postId);
+    public ResponseEntity<PostResponse> findAll(@PathVariable Long postId) {
+        PostResponse response = postService.findById(postId);
         return ResponseEntity.ok(response);
     }
 
--- backend/src/main/java/com/BE_13/DevTogether/entity/user/User.java
+++ backend/src/main/java/com/BE_13/DevTogether/entity/user/User.java
@@ -1,53 +1,41 @@
-package com.BE_13.DevTogether.entity.user;
-
-import com.BE_13.DevTogether.entity.comment.Comment;
-import com.BE_13.DevTogether.entity.posts.Post;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * 유저(User) 엔티티(Entity) 클래스이다.
- *
- * @author chan
- */
-@Getter
-@Table(name = "Users")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
-@Entity
-public class User {
-
-    // TODO: private UserId id 로 바뀔 수 있다.
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 유저 아이디
-
-    private String username; // 작성자
-
-    @Enumerated(EnumType.STRING)
-    private Role role; // 유저 권한
-
-    @OneToMany(mappedBy = "author")
-    private List<Post> uploadedPosts = new ArrayList<>(); // 유저가 작성한 포스트 리스트
-
-    @OneToMany(mappedBy = "author")
-    private List<Comment> uploadedComment = new ArrayList<>(); // 유저가 작성한 댓글 리스트
-
-    @CreatedDate
-    private LocalDateTime createDate; // 생성일자
-
-    @Builder
-    public User(String username, Role role) {
-        this.username = username;
-        this.role = role;
-    }
-}
+//package com.BE_13.DevTogether.entity.user;
+//
+//import jakarta.persistence.Id;
+//
+//import java.time.LocalDateTime;
+//
+///**
+// * 유저(User) 엔티티(Entity) 클래스이다.
+// *
+// * @author chan
+// */
+//public class User {
+//
+//    // TODO: private UserId id 로 바뀔 수 있다.
+//    @Id
+//    private Long id; // 유저 아이디
+//
+//    private String username; // 작성자
+//
+//    private LocalDateTime createDate; // 생성일자
+//
+//    // TODO: private Role role 으로 바뀔 수 있다.
+//    private String role; // 유저 권한
+//
+//    // Getter
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public LocalDateTime getCreateDate() {
+//        return createDate;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//}
