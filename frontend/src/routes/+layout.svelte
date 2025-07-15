383162854e414836434ab1ab9ef083dd8a4e2adb
MiSa-JD <jeongs0427@gmail.com>
2025-07-15 17:03:58 +0900

    added login / signup page, moved write page to /board/write page.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,17 +2,6 @@
   import '../app.css'
 </script>
 
-<!-- 헤더 -->
-<!-- TODO: 얘를 routes/의 레이어웃으로 쓰지 말고 따로 분리해버릴까? 헤더라는 컴포넌트로 분리해서 관리하고? -->
-<header class="bg-gray-100 py-4 px-6 text-xl font-bold flex justify-between drop-shadow-sm">
-  <a href="/">
-    DevTogether
-  </a>
-  <a href="/login">
-    로그인
-  </a>
-</header>
-
 <slot />
 
 <footer></footer>
--- frontend/src/routes/login/+page.svelte
+++
@@ -1,9 +0,0 @@
-<ScreenModal>
-  로그인 페이지 테스트
-</ScreenModal>
-
-
-
-<script>
-  import ScreenModal from "$lib/components/screenModal/screenModal.svelte";
-</script>
